f9757d45ef95eb94149483b935bbc910886cfd55
abel <abelhailu0427@gmail.com>
2024-12-30 01:37:31 -0800

    budget-tracker

---
+++ budget-tracker/src/components/ViewBudget.js
@@ -0,0 +1,29 @@
+// import React from 'react';
+
+// const ViewBudget = (props) => {
+// 	return (
+// 		<>
+// 			<span>Budget: £{props.budget}</span>
+// 			<button type='button' class='btn btn-primary' onClick={props.handleEditClick}>
+// 				Edit
+// 			</button>
+// 		</>
+// 	);
+// };
+
+// export default ViewBudget;    
+
+
+
+
+// "@testing-library/jest-dom": "^5.17.0",
+//     "@testing-library/react": "^13.4.0",
+//     "@testing-library/user-event": "^13.5.0",
+//     "bootstrap": "^5.3.3",
+//     "react": "^18.3.1",
+//     " ": "^18.3.1",
+//     "react-icons": "^5.2.1",
+//     "react-scripts": "5.0.1",
+//     "uuid": "^10.0.0",
+//     // "web-vitals": "^2.1.4"
+
